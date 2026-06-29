c559ad58af8f531559244432af0e8f0167c75861
simranjeet kour <simranjeetkour8407@gmail.com>
2026-06-29 09:09:49 +0530

    Fix Vercel deployment and add vercel.json

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config()
 connectDB()
 
 const app = express()
-const PORT = 4000 
+const PORT =4000
 
 
 // middleware
@@ -17,6 +17,10 @@ app.use(cors({
     origin :'http://localhost:5173',
     credentials : true
 }))
+
+app.get('/', (req,res) => {
+    res.send('Backend is Running successfully!')
+})
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
