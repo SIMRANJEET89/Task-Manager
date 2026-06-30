c5557840e0eedbbb601e6cb88ea9cbe26e2c7244
simranjeet kour <simranjeetkour8407@gmail.com>
2026-06-30 08:46:51 +0530

    Replace local diskStorage with Cloudinary storage

--- backend/Middleware/multer.js
+++ backend/Middleware/multer.js
@@ -1,15 +1,23 @@
+import {v2 as cloudinary} from 'cloudinary'
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer'
 import path from 'path'
 
-const storage = multer.diskStorage({
-    destination : (req, file, cb) => {
-        cb(null, 'uploads/')
-    },
+// Cloudinary credentials configuration
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+})
 
-    filename: (req, file ,cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
+//Create Cloudinary storage 
+const storage = new CloudinaryStorage({
+    cloudinary : cloudinary,
+    params : {
+        folder : 'Task-manager-avatar',
+        allowed_formats : ['jpg', 'png', 'jpeg']
     }
-});
-const upload = multer({ storage: storage})
+})
+const upload = multer({ storage : storage});
 
 export default upload
