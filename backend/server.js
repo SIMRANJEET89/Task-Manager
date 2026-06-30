ae5f2f4777018c8244883ea8bd5ebab1d609e2a6
simranjeet kour <simranjeetkour8407@gmail.com>
2026-06-30 08:56:34 +0530

    Fix Express v5 CORS options path error

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
-app.options('*', cors());
+app.options('/(.*)', cors());
 
 app.get('/', (req,res) => {
     res.send('Backend is Running successfully!')
